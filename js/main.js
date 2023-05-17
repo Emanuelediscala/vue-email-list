00cbc604ab01656b2b9ad08ccc4172c3fd744beb
Emanuele discala <manueldiscala@gmail.com>
2023-05-17 18:55:57 +0200

    end + bonus

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-
+            listaMail:[]
         }
     },
-    methods : {
-
+    methods: {
+        ciclogenerazioneMail() {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((risposta => {
+                    this.listaMail.push(risposta.data.response)
+                    ;
+                }))
+            }
+        }
     },
     mounted() {
-        console.log();
-
+    this.ciclogenerazioneMail()
     }
 }).mount("#app")
